49fdf8cd774f8c4946d1df201d8900b85ce2249d
mohit <mk8205897@gmail.com>
2019-09-09 05:31:17 +0530

    this commit from US Timezone guy

--- src/test/java/ingenioustechhub/TestRestAPI.java
+++ src/test/java/ingenioustechhub/TestRestAPI.java
@@ -6,6 +6,8 @@ public class TestRestAPI {
 	@Test
 	public void postJira() {
 		System.out.println("PostJira");
+		System.out.println("PostJira1");
+		System.out.println("PostJira2");
 	}
 	@Test
 	public void deleteTwwitter() {
